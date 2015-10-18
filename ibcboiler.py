949484284c1f4fe8c0db88cf8c8aae6dcdced7e8
grevian <grevian+github@gmail.com>
2015-10-17 21:50:13 -0400

    Initial Commit

---
+++ ibcboiler.py
@@ -0,0 +1,150 @@
+import json
+import requests
+import datetime
+import logging
+
+
+def to_celcius(value):
+    return (value - 32)/1.8
+
+
+class IBCBoiler(object):
+
+    class Temperature(object):
+        Celcius = 1
+        Fahrenheight = 2
+
+    class Status(object):
+        Heating = "Heating"
+        Standby = "Standby"
+        Purging = "Purging"
+        Igniting = "Igniting"
+        Initializing = "Initializing"
+
+    class ObjectCodes(object):
+        IBC_MASTER_BOILER_DATA = 2
+        IBC_BOILER_STATUS_DATA = 3
+        IBC_BOILER_RUN_PROFILE_DATA = 5
+        IBC_BOILER_LOG_DATA = 6
+        IBC_BOILER_BOILER_DATA = 11
+        IBC_BOILER_STANDARD_DATA = 13
+        IBC_BOILER_ADV_SETTINGS_DATA = 15
+        IBC_BOILER_EXT_DETAIL_DATA = 19
+        IBC_CLOCK_DATA = 24
+        IBC_BOILER_SITE_DATA = 34
+
+        # --- Advanced settings, things like load pairing information not useful to most people
+        IBC_LOAD_PAIRING_DATA = 25
+        # TODO will this tell me when I need to run a cleaning cycle?, what does that entail?
+        IBC_BOILER_CLEANING_SETTINGS_DATA = 18
+        IBC_BOILER_MULTI_SETTINGS_DATA = 17
+
+        # --- These fail when hit with the parameters I was using, more discovery needed
+        # Example response { "object_no": 201, "fail_code": 1, "operation": 7, "object_index": -1 }
+        IBC_BOILER_ERRLOG_DATA = 7
+        IBC_BOILER_SETBACK_DATA = 14
+        IBC_BOILER_LOAD_SETTINGS_DATA = 16
+        IBC_SITELOG_DATA = 23
+        IBC_BOILER_CAPTURE_DATA = 26
+
+        # --- DO NOT USE --- (These are either for setting values, which is not supported with this class, or look
+        # dangerous enough I didn't want to hit them to find out
+        # IBC_PASSWORD_DATA = 99
+        # IBC_ALERT_DATA = 31
+        # IBC_BOILER_BACKUP_ADVANCED = 27 # Discovered by me, http://192.168.2.13/js/adv.js:4
+        # IBC_BOILER_RESTORE_ADVANCED = 28 # Discovered by me, http://192.168.2.13/js/adv.js:5
+        # IBC_BOILER_RESTORE = 29
+        # IBC_BOILER_FACTORY_DATA = 20
+        # IBC_BOILER_FACTORY_SETTINGS_DATA = 21
+
+    # Represents values that are not set (Disconnected sensors, etc.)
+    _UNSET_SENTINEL_VALUE = 32766
+
+    # Keep people from spamming the boiler in too quick of a loop
+    MINIMUM_DELAY = datetime.timedelta(seconds=9)
+    LAST_REQUEST = None
+
+    TARGET_URL = "cgi-bin/bc2-cgi"
+
+    def __init__(self, address, temperature_type=Temperature.Celcius):
+        self.address = address
+
+        self.temperature_flag = temperature_type
+        self._status = None
+        self._supply_temperature = None
+        self._return_temperature = None
+        self._target_temperature = None
+        self._inlet_pressure = None
+        self._delta_pressure = None
+        self._mbh = None
+
+        self.target = "http://%s/%s" % (self.address, self.TARGET_URL)
+
+        # TODO Maybe hit one of the boiler info rpcs for the name/number etc.
+
+    def _request_object(self, object):
+        # TODO Cache and return the response instead of raising an exception
+        # if self.LAST_REQUEST and self.LAST_REQUEST + self.MINIMUM_DELAY < datetime.datetime.now():
+        #     raise Exception("Too many requests too fast!")
+
+        self.LAST_REQUEST = datetime.datetime.now()
+        # TODO not very pretty, but this is the actual format... json as a parameter
+        # TODO object_no: 100 is SUPER IMPORTANT. it's the container for all the information,
+        # Otherwise you're attempting to SET that information with the query values
+        payload = {"json": json.dumps({"object_no": 100, "object_request": object, "boiler_no": 0})}
+        response = requests.get(self.target, params=payload)
+
+        logging.debug(response.url)
+        # logging.debug(response.text)
+
+        return response
+
+    def refresh(self):
+        response = self._request_object(IBCBoiler.ObjectCodes.IBC_BOILER_EXT_DETAIL_DATA)
+        self._update(
+            response.json()
+        )
+
+    def _update(self, values):
+        self._status = values.get("Status", "Unknown")
+        self._mbh = values.get("MBH")
+        # IBC Stores the temperature as 4x for some reason
+        # Store the values internally as regular Celsius values
+        self._supply_temperature = values.get("SupplyT")/4
+        self._return_temperature = values.get("ReturnT")/4
+        self._target_temperature = values.get("TargetT")/4
+        self._inlet_pressure = values.get("InletPressure")
+        self._delta_pressure = values.get("DeltaPressure")
+
+    @property
+    def status(self):
+        return self._status
+
+    @property
+    def mbh(self):
+        return self._mbh
+
+    @property
+    def supply_temperature(self):
+        if self.temperature_flag == IBCBoiler.Temperature.Celcius:
+            return self._supply_temperature
+        else:
+            raise NotImplementedError()
+
+    @property
+    def return_temperature(self):
+        if self.temperature_flag == IBCBoiler.Temperature.Celcius:
+            return self._return_temperature
+        else:
+            raise NotImplementedError()
+
+    @property
+    def target_temperature(self):
+        if self.temperature_flag == IBCBoiler.Temperature.Celcius:
+            return self._target_temperature
+        else:
+            raise NotImplementedError()
+
+    @property
+    def inlet_pressure(self):
+        return self._inlet_pressure
---
+++ main.py
@@ -0,0 +1,87 @@
+import json
+import requests
+from requests import ConnectionError, Timeout
+from ibcboiler import IBCBoiler
+import logging
+import datetime
+import time
+import argparse
+
+logging.basicConfig(level=logging.DEBUG)
+
+parser = argparse.ArgumentParser(description='Read data from an IBC Boiler and send it to a web based monitoring server')
+parser.add_argument('--boiler_ip', type=str, help='The boilers IP Address')
+
+parser.add_argument('--monitor_address', type=str, help='The monitoring server hostname/address')
+parser.add_argument('--secret', type=str, help='The secret to include in status updates')
+
+args = parser.parse_args()
+
+STANDBY_DELAY = datetime.timedelta(minutes=5)
+SENSOR_URL = args.monitor_address
+
+# Ensure an update is done at least on the first loop iteration, no matter the boiler status
+last_update = datetime.datetime.now() - STANDBY_DELAY
+
+# TODO Read from a configuration file/the command line
+i = IBCBoiler(args.boiler_ip)
+request_payload = {
+    "sensor": "IBCBoiler",
+    "secret": args.secret
+}
+
+
+def send_sensor_data(payload):
+    try:
+        r = requests.post(SENSOR_URL, data=json.dumps(payload))
+    except (ConnectionError, Timeout) as e:
+        logging.warn("Failed to send update to the server: %r", e)
+        return
+
+    if r.status_code != 200:
+        logging.error("Server error, %r" % r.content)
+
+
+def send_temperature_update(temperature_type, value):
+    payload = request_payload.copy()
+    payload["type"] = "boiler_temperature"
+    payload["subtype"] = temperature_type
+    payload["value"] = value
+    send_sensor_data(payload)
+
+
+def send_mbh_update(value):
+    payload = request_payload.copy()
+    payload["type"] = "mbh"
+    payload["value"] = value
+    send_sensor_data(payload)
+
+
+def send_boiler_update(boiler):
+    send_temperature_update("supply", boiler.supply_temperature)
+    send_temperature_update("return", boiler.return_temperature)
+    send_temperature_update("target", boiler.target_temperature)
+    send_mbh_update(boiler.mbh)
+
+
+while True:
+    logging.debug("Updating Boiler Information")
+    i.refresh()
+
+    if i.status in (IBCBoiler.Status.Heating,
+                    IBCBoiler.Status.Purging,
+                    IBCBoiler.Status.Igniting,
+                    IBCBoiler.Status.Initializing):
+        # Update immediately, values change very quickly while heating
+        last_update = datetime.datetime.now()
+        send_boiler_update(i)
+    elif i.status == IBCBoiler.Status.Standby:
+        # Only update if a lot of time has passed, updates during standby are much less significant
+        if last_update + STANDBY_DELAY <= datetime.datetime.now():
+            last_update = datetime.datetime.now()
+            send_boiler_update(i)
+    else:
+        # Log a warning here, maybe an error/off state?
+        logging.warning("Boiler is in an unexpected state: %r", i.status)
+
+    time.sleep(10)
